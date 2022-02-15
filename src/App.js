d68bb957f559c2de53ae63265c5c4cb49bf87b14
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-16 01:47:49 +0400

    feat/Create-routes-and-page-components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Cont from './pages/container/Container';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="ImgStyle2" src="./Ellipse 5.svg"/>
+      <img  className="ImgStyle" src="./Mask Group (1).png"/>
+      <img  className="ImgStyle1" src="./Mask Group (2).png"/>
+      <Cont/>
+
+    
     </div>
   );
 }
---
+++ src/pages/tv-shows/Tv-shows.jsx
@@ -0,0 +1,8 @@
+
+function TvShows(){
+    return <h1>
+        <img style ={{marginTop:'30px'}} src="https://sophiaismaa.files.wordpress.com/2018/05/tv-shows.jpg"/>
+       
+    </h1>
+}
+export default TvShows
