2a1883001f1a38c67bc718edc23af4c03b84819b
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-16 12:00:59 +0400

    feat:Create-routes-and-page-components

--- src/pages/container/Container.jsx
+++ src/pages/container/Container.jsx
@@ -15,6 +15,7 @@ function Cont() {
                         <div className="Links">
                             <Link to="/movies">Movies</Link>
                             <Link style={{marginLeft:"10px"}} to="/tv_shows">Tv-shows</Link>
+                            <Link  style={{marginLeft:"10px"}}to="/">Home</Link>
                         </div>
                         <Routes >
                             <Route path="/movies" element={<Movies />} />
@@ -54,12 +55,12 @@ function Cont() {
                         <img src="./imdb-icon.png"/>
                         <img src="./10.png"/>
                         </div>
-                        {/* <h3>
+                        <h3>
                             Overview
-                            </h3> */}
-                        {/* <div className="Text">
+                            </h3> 
+                        <div className="Text">
                             
-                            {/* <p>
+                             <p>
                             Istanbul Police Department homicide detectives encounter a murder 
                             nothing like they’ve seen before.Commissioners Emin, Salih, Asuman
                              and deputy Alaattin begin investigating.But strange murders keep happening. 
@@ -71,8 +72,8 @@ function Cont() {
                                 panic, pressure and disdain, Emin and his friends try to solve the
                                  strangest series of murders with the most unusual methods
                             .This fast race against time immediately turns into a tragicomedy.
-                            </p> */}
-                        {/* </div>  */}
+                            </p> 
+                        </div> 
 
                     </div>
 
