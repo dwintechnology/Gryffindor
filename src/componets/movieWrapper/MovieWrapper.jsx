7fb061a91bf61c2113cc458a0b3aa42b8917f3a5
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-02-21 14:18:08 +0400

    Create Movie Detals

--- src/componets/movieWrapper/MovieWrapper.jsx
+++ src/componets/movieWrapper/MovieWrapper.jsx
@@ -2,7 +2,7 @@ import './style.scss'
  import MovieDetails from "../movie-details/movie-details"
 function MovieWrapper(){
     return( 
-    <div className="movshow">
+    <div className="movieshow">
         <div style={{width:'30%',height:"30px",marginRight:'10%'}}>aaa</div>
         <MovieDetails/>
     </div>
