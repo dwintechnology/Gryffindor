abda126ed873580c455ce29327d46dcab3d9165d
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-02-21 14:15:58 +0400

    Create Movie Detals

--- src/componets/movie-details/movie-actor/create-movie-actor/create-movie-actor.js
+++ src/componets/movie-details/movie-actor/create-movie-actor/create-movie-actor.js
@@ -2,15 +2,16 @@ import "./create-movie-actor.scss"
 // import imgSrc from '../../../../assets/img/Ellipse1.svg'
 
 function CreateMovieActor(props) {
-    const className = props.className
-    const foto = props.foto
+    const name = props.name
+    const photo = props.photo
+    const filmName = props.filmName
 
 
     return (
         <div className="actorBlock">
-            <img src={foto} />
-            <h3>{className}</h3>
-            <h5>Film Name</h5>
+            <img src={photo} />
+            <h3>{name}</h3>
+            <h5>{filmName}</h5>
         </div>
     )
 }
--- src/componets/movie-details/movie-actor/movie-actor.js
+++ src/componets/movie-details/movie-actor/movie-actor.js
@@ -2,21 +2,24 @@ import CreateMovieActor from './create-movie-actor/create-movie-actor'
 import './movie-actor.scss'
 
 
-function MovieActor(){
-const a =[{name:"barev",
-            foto:'https://resizer.mail.ru/p/614d64e0-aad1-5e73-bf30-862513dfc0c4/AAACUSw4NYQyJQeHy7YhKeoQUZvS__1TI5tDHoFjA3FcAp67ths9lzr7tKAKZTfB_3P3sdpn7W0Bh7AfESH-beTZN-M.jpg'
-            },
+function MovieActor() {
+    const a = [{
+        name: "barev",
+        photo: 'https://resizer.mail.ru/p/614d64e0-aad1-5e73-bf30-862513dfc0c4/AAACUSw4NYQyJQeHy7YhKeoQUZvS__1TI5tDHoFjA3FcAp67ths9lzr7tKAKZTfB_3P3sdpn7W0Bh7AfESH-beTZN-M.jpg',
+        filmName: "Arev"
+    },
 
-            {name:"hajox",
-            foto:'https://resizer.mail.ru/p/614d64e0-aad1-5e73-bf30-862513dfc0c4/AAACUSw4NYQyJQeHy7YhKeoQUZvS__1TI5tDHoFjA3FcAp67ths9lzr7tKAKZTfB_3P3sdpn7W0Bh7AfESH-beTZN-M.jpg',
-        }
-        ]
-    return(
+    {
+        name: "hajox",
+        photo: 'https://resizer.mail.ru/p/614d64e0-aad1-5e73-bf30-862513dfc0c4/AAACUSw4NYQyJQeHy7YhKeoQUZvS__1TI5tDHoFjA3FcAp67ths9lzr7tKAKZTfB_3P3sdpn7W0Bh7AfESH-beTZN-M.jpg',
+        filmName: "Karen",
+    }
+    ]
+    return (
         <div className="movieActor">
-            {a.map((element,i) => {
-              return  <CreateMovieActor key={i} className={element.name} foto={element.foto}/>
-                
-            })};
+            {a.map((element, i) => {
+                return <CreateMovieActor key={i} name={element.name} photo={element.photo} filmName={element.filmName} />
+            })}
 
         </div>
     )
