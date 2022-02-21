cb48730e3a42cd1da4e00bb5920fa3146f49c2b2
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-02-21 12:58:07 +0400

    Create Movie Detals

--- src/componets/movie-details/movie-actor/create-movie-actor/create-movie-actor.js
+++ src/componets/movie-details/movie-actor/create-movie-actor/create-movie-actor.js
@@ -1,15 +1,17 @@
 import "./create-movie-actor.scss"
-import imgSrc from '../../../../assets/img/Ellipse1.svg'
+// import imgSrc from '../../../../assets/img/Ellipse1.svg'
 
-function CreateMovieActor(props){
-    const className=props.className
+function CreateMovieActor(props) {
+    const className = props.className
+    const foto = props.foto
 
-    return(
-    <div className="actorBlock">
-        <img src={imgSrc}/>
-        <h3>{className}</h3>
-        <h5>Film Name</h5>
-    </div>
+
+    return (
+        <div className="actorBlock">
+            <img src={foto} />
+            <h3>{className}</h3>
+            <h5>Film Name</h5>
+        </div>
     )
 }
 export default CreateMovieActor
--- src/componets/movie-details/movie-actor/movie-actor.js
+++ src/componets/movie-details/movie-actor/movie-actor.js
@@ -1,12 +1,20 @@
 import CreateMovieActor from './create-movie-actor/create-movie-actor'
 import './movie-actor.scss'
 
+
 function MovieActor(){
-const a =["barev","hajox","vonch es"]
+const a =[{name:"barev",
+            foto:'https://resizer.mail.ru/p/614d64e0-aad1-5e73-bf30-862513dfc0c4/AAACUSw4NYQyJQeHy7YhKeoQUZvS__1TI5tDHoFjA3FcAp67ths9lzr7tKAKZTfB_3P3sdpn7W0Bh7AfESH-beTZN-M.jpg'
+            },
+
+            {name:"hajox",
+            foto:'https://resizer.mail.ru/p/614d64e0-aad1-5e73-bf30-862513dfc0c4/AAACUSw4NYQyJQeHy7YhKeoQUZvS__1TI5tDHoFjA3FcAp67ths9lzr7tKAKZTfB_3P3sdpn7W0Bh7AfESH-beTZN-M.jpg',
+        }
+        ]
     return(
         <div className="movieActor">
             {a.map((element,i) => {
-              return  <CreateMovieActor key={i} className={element}/>
+              return  <CreateMovieActor key={i} className={element.name} foto={element.foto}/>
                 
             })};
 
