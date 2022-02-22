6dad476842462d4cee31c27d00e513c037e5a17b
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-02-22 17:33:22 +0400

    transition mobile version

--- src/componets/shades/movies/Movies.jsx
+++ src/componets/shades/movies/Movies.jsx
@@ -1,12 +1,13 @@
-
 import './style.scss';
 import PropTypes from 'prop-types';
+
 function Movies({isTvShow}){
     console.log(isTvShow)
     return <h1>
        
     </h1>
 }
+
 Movies.propTypes = {
     isTvShow: PropTypes.bool}
 export default Movies
