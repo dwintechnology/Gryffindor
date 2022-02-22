03ef34e28eafce168f7aebe4486d1d3719ecdb3a
ArmanKhoyetsyan <arman.khoyetsyan.dwin@gmail.com>
2022-02-22 14:38:05 +0400

    switch to mobile version

--- src/componets/movieWrapper/MovieWrapper.jsx
+++ src/componets/movieWrapper/MovieWrapper.jsx
@@ -1,26 +1,23 @@
-import './style.scss'
- import MovieDetails from "../movie-details/movie-details"
-import MovieCover from '../moviecover/MovieCover';
-import SuggestBtn from '../suggestionbtn/SuggestionBtn';
-import NetflixButton from '../netflixButton/NetflixButton';
+import "./style.scss";
+import MovieDetails from "../movie-details/movie-details";
+import MovieCover from "../moviecover/MovieCover";
+import SuggestBtn from "../suggestionbtn/SuggestionBtn";
+import NetflixButton from "../netflixButton/NetflixButton";
 
 function MovieWrapper() {
+  const handleClick = function () {
+    window.location.reload();
+  };
 
-    const handleClick = function () {
-        window.location.reload()
-    }
-
-    return (
-        <div className="movieshow">
-            <div className="picAndBtn">
-                <MovieCover />
-                <NetflixButton />
-                <SuggestBtn onClick={handleClick} />
-                
-            </div>
-            <MovieDetails/>
-        </div>
-    )
+  return (
+    <div className="movieshow">
+      <div className="picAndBtn">
+        <MovieCover />
+        <NetflixButton />
+        <SuggestBtn onClick={handleClick} />
+      </div>
+        <MovieDetails />
+    </div>
+  );
 }
 export default MovieWrapper;
-
