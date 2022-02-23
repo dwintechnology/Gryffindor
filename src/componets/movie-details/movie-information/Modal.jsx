f53a21af2465e8d75f825f75bb88a5488be85665
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-23 16:57:01 +0400

    create modal window and fetch youtube search--API

--- src/componets/movie-details/movie-information/Modal.jsx
+++ src/componets/movie-details/movie-information/Modal.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './modal.scss';
-import PlayerLogo from "../../../assets/img/PlayerLogo.png";
+import PlayerLogo from '../../../assets/img/PlayerLogo.png';
 
-const PopupExample = ({videoId}) => (
+const PopupModal = ({videoId}) => (
   
-  <Popup trigger={<button  className ="trailerPlay1" > <img src={PlayerLogo}/>Play Trailer</button>} position="Top-left">
+  <Popup trigger={<button  className ='trailerPlay1' > <img src={PlayerLogo}/>Play Trailer</button>} position='Top-left'>
     {close => (
       <div>
-        <iframe className="video" width="670px" height="auto" src={`https://www.youtube.com/embed/${videoId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <button className="close" onClick={close}>
+        <iframe className='video' width='670px' height='auto' src={`https://www.youtube.com/embed/${videoId}`} title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+        <button className='close' onClick={close}>
           X
         </button>
       </div>
@@ -19,5 +19,5 @@ const PopupExample = ({videoId}) => (
 );
 
 
-export default PopupExample
+export default PopupModal
 
--- src/componets/movie-details/movie-information/movie-information.js
+++ src/componets/movie-details/movie-information/movie-information.js
@@ -1,22 +1,17 @@
-import "./movie-information.scss";
-import PopupExample from "./Modal";
-import { useEffect, useState } from "react";
+import './movie-information.scss';
+import React, { useState, useEffect } from 'react';
+import PopupModal from './Modal';
+
 function MovieInformation() {
-  let [castobj, setCastObj] = useState();
-  let title = "Spider Man";
-  function getDATA() {
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC_mymmew6S2GNkUc1el0HFTYdKKhS0EwQ&type=video&q=${title} + "triller"`
-    )
-      .then((first) => {
-        return first.json();
-      })
-      .then((last) => {
-        setCastObj(last);
-      });
+  const [vidId, setVidId] = useState();
+  const title = 'Spider Man';
+  function getData() {
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC_mymmew6S2GNkUc1el0HFTYdKKhS0EwQ&type=video&q=${title} + 'triller'`)
+      .then((data) => data.json())
+      .then((apiData) => setVidId(apiData));
   }
   useEffect(() => {
-    getDATA();
+    getData();
   }, []);
   return (
     <div className="movieInformation">
@@ -25,7 +20,7 @@ function MovieInformation() {
       <div className="movieTime">1h 29m</div>
       <div className="trailerPlay">
         <div>
-          <PopupExample videoId={castobj?.items[0].id.videoId} />
+          <PopupModal videoId={vidId?.items[0].id.videoId} />
         </div>
       </div>
     </div>
