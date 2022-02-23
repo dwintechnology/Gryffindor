c9e1c81e388f4ba956cfcdb2e86176947a5ca835
Maria200011 <maria.jinbashyan.dwin@gmail.com>
2022-02-23 15:02:19 +0400

    create modal window and fetch youtube search  API

--- src/componets/movie-details/movie-information/movie-information.js
+++ src/componets/movie-details/movie-information/movie-information.js
@@ -1,19 +1,29 @@
- import "./movie-information.scss"
- import PlayerLogo from "../../../assets/img/PlayerLogo.png" 
+import "./movie-information.scss";
+import PopupExample from "./Modal";
+import { useEffect, useState } from "react";
+function MovieInformation() {
+  let [castobj, setCastObj] = useState()
+  let title = "Spider Man"
+    function getDATA(){
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC_mymmew6S2GNkUc1el0HFTYdKKhS0EwQ&type=video&q=${title} + "triller"`)
+        .then((first) => { return first.json() })
+        .then((last) => { setCastObj(last) })
+    }
+    useEffect(()=>{
+        getDATA()
+    }, [])
+  return (
+    <div className="movieInformation">
+      <div className="movieReleaseDate">12/25/2255</div>
+      <div className="movieGenre">Comedy, Crime</div>
+      <div className="movieTime">1h 29m</div>
+      <div className="trailerPlay">
+        <div>
+          <PopupExample videoId={castobj?.items[0].id.videoId}/>
+        </div>
+      </div>
+    </div>
+  );
+}
 
- function MovieInformation (){
-     return(
-         <div className="movieInformation">
-             <div className="movieReleaseDate">12/25/2255</div>
-             <div className="movieGenre">Comedy, Crime</div>
-             <div className="movieTime">1h 29m</div>
-             <div className="trailerPlay">Play Trailer 
-             <img src={PlayerLogo}/>
-
-             </div>
-
-         </div>
-     )
- }
-
- export default MovieInformation
+export default MovieInformation;
